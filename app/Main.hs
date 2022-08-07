12ceec74eca4d1ef5da3219618538757905fe5ae
Liu Zhenwei <liuzw1951@outlook.com>
2022-08-08 07:36:00 +0800

    format

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
 data Tower = A | B | C deriving (Show,Eq)
-data Action = Act Tower Tower deriving (Eq)
+data Action = Act Tower Tower
+  deriving Eq
 instance (Show Action) where
-    show (Act a b) = show a ++ "->" ++ show b
+  show (Act a b) = show a ++ "->" ++ show b
 move :: Tower -> Tower -> Action
 move = Act
 hanoi :: Int -> Tower -> Tower -> Tower -> [Action]
@@ -11,9 +12,9 @@ hanoi 1 a b c = [move a c]
 hanoi n a b c = hanoi (n - 1) a c b ++ [move a c] ++ hanoi (n - 1) b a c
 main :: IO ()
 main = putItem $ hanoi 3 A B C
-  where
-    putItem (x : xs)
-        | xs == [] = print x
-        | xs /= [] = do
-            print x
-            putItem xs
+ where
+  putItem (x : xs)
+    | xs == [] = print x
+    | xs /= [] = do
+      print x
+      putItem xs
