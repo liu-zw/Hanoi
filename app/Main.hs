c7d1a2b1f33e57d2ea2cfebae441b2abe4ab112b
Liu Zhenwei <liuzw1951@outlook.com>
2022-08-08 10:47:17 +0800

    add readme and change main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 module Main where
+import           Text.Read
 
 data Tower = A | B | C deriving (Show,Eq)
 data Action = Act Tower Tower
@@ -11,7 +12,15 @@ hanoi :: Int -> Tower -> Tower -> Tower -> [Action]
 hanoi 1 a b c = [move a c]
 hanoi n a b c = hanoi (n - 1) a c b ++ [move a c] ++ hanoi (n - 1) b a c
 main :: IO ()
-main = putItem $ hanoi 3 A B C
+main = do
+  putStrLn "Input a number."
+  numS <- getLine
+  let num = readMaybe numS :: Maybe Int
+  case num of
+    Nothing -> putStrLn "It's not a number!"
+    Just x  -> do 
+      putStrLn "The solution is:"
+      putItem $ hanoi x A B C
  where
   putItem (x : xs)
     | xs == [] = print x
